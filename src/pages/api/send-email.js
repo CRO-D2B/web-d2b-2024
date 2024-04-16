b3ea1abf12c3f6ada5f9410a06b0ab4ad9d58278
FabianMorag <fabianmorag0405@gmail.com>
2024-04-16 12:46:48 -0400

    send email endpoint

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -1,22 +1,27 @@
 import { Resend } from 'resend'
+import { isDev } from '@/lib/utils'
+
+const devTag = isDev ? 'DEV | ' : ''
 
 export const POST = async ({ params, request }) => {
   const { name, email, number, company, message } = await request.json()
 
   const resend = new Resend(import.meta.env.RESEND_API_KEY)
-
-  resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: 'fabianmorag0405@gmail.com',
-    subject: 'DEV | Hello World',
-    html: `
-      <strong>Nombre</strong>: ${name} <br>
-      <strong>Email</strong>: ${email} <br>
-      <strong>Telefono</strong>: ${number} <br>
-      <strong>Empresa</strong>: ${company} <br>
-      <strong>Comentario</strong>: ${message} <br>
-    `
-  })
-
-  return new Response('OK', { status: 200, headers: { 'Content-Type': 'text/plain' } })
+  try {
+    resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: 'fabianmorag0405@gmail.com',
+      subject: `${devTag}Hello World`,
+      html: `
+        <strong>Nombre</strong>: ${name} <br>
+        <strong>Email</strong>: ${email} <br>
+        <strong>Telefono</strong>: ${number} <br>
+        <strong>Empresa</strong>: ${company} <br>
+        <strong>Comentario</strong>: ${message} <br>
+      `
+    }).then(res => console.log(res)).catch(err => console.log(err))
+    return new Response('OK', { status: 200, headers: { 'Content-Type': 'text/plain' } })
+  } catch (e) {
+    return new Response('Error', { status: 500, headers: { 'Content-Type': 'text/plain' } })
+  }
 }
--- src/services/services.js
+++ src/services/services.js
@@ -1,19 +1,29 @@
+import { isProd } from '@/lib/utils'
+
 const API_URL = 'https://admin.d2b.cl/d2badmin'
+const publishedFilter = isProd ? 'status=published' : ''
 
 export const getServicesInfo = async () => {
-  const res = await fetch(`${API_URL}/items/2024_servicios`)
+  const url = buildUrl({ endpoint: '/items/2024_servicios' })
+  const res = await fetch(url)
   const { data } = await res.json()
   return data
 }
 
 export const getServiceInfoBySlug = async ({ serviceSlug }) => {
-  const res = await fetch(`${API_URL}/items/2024_servicios?filter[slug]=${serviceSlug}`)
+  const url = buildUrl({ endpoint: '/items/2024_servicios', params: `filter[slug]=${serviceSlug}` })
+  const res = await fetch(url)
   const { data } = await res.json()
   return data[0]
 }
 
 export const getServicesResume = async () => {
-  const res = await fetch(`${API_URL}/items/2024_servicios?fields[]=nombre_del_servicio&fields[]=slug`)
+  const url = buildUrl({ endpoint: '/items/2024_servicios', params: 'fields[]=nombre_del_servicio&fields[]=slug' })
+  const res = await fetch(url)
   const { data } = await res.json()
   return data
+}
+
+const buildUrl = ({ endpoint, params = '' }) => {
+  return `${API_URL}${endpoint}?${publishedFilter}&${params}`
 }
