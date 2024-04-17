85af6cd4c80da815796ae9e774edba78d2f4df2d
FabianMorag <fabianmorag0405@gmail.com>
2024-04-17 16:42:37 -0400

    fetch content

---
+++ src/services/blog.js
@@ -0,0 +1,23 @@
+const WP_API_URL = 'https://d2b.cl/blog/wp-json/wp/v2'
+
+export const getCategories = async () => {
+  const endpoint = '/categories'
+  const params = '_fields=name,slug'
+  const url = buildUrl({ endpoint, params })
+  const res = await fetch(url)
+  const data = await res.json()
+  return data
+}
+
+export const getPosts = async () => {
+  const endpoint = '/posts'
+  const params = 'per_page=6'
+  const url = buildUrl({ endpoint, params })
+  const res = await fetch(url)
+  const data = await res.json()
+  return data
+}
+
+const buildUrl = ({ endpoint, params = '' }) => {
+  return `${WP_API_URL}${endpoint}?${params}`
+}
--- src/services/services.js
+++ src/services/services.js
@@ -4,21 +4,26 @@ const API_URL = 'https://admin.d2b.cl/d2badmin'
 const publishedFilter = isProd ? 'status=published' : ''
 
 export const getServicesInfo = async () => {
-  const url = buildUrl({ endpoint: '/items/2024_servicios' })
+  const endpoint = '/items/2024_servicios'
+  const url = buildUrl({ endpoint })
   const res = await fetch(url)
   const { data } = await res.json()
   return data
 }
 
 export const getServiceInfoBySlug = async ({ serviceSlug }) => {
-  const url = buildUrl({ endpoint: '/items/2024_servicios', params: `filter[slug]=${serviceSlug}` })
+  const endpoint = '/items/2024_servicios'
+  const params = `filter[slug]=${serviceSlug}`
+  const url = buildUrl({ endpoint, params })
   const res = await fetch(url)
   const { data } = await res.json()
   return data[0]
 }
 
 export const getServicesResume = async () => {
-  const url = buildUrl({ endpoint: '/items/2024_servicios', params: 'fields[]=nombre_del_servicio&fields[]=slug' })
+  const endpoint = '/items/2024_servicios'
+  const params = 'fields[]=nombre_del_servicio&fields[]=slug'
+  const url = buildUrl({ endpoint, params })
   const res = await fetch(url)
   const { data } = await res.json()
   return data
