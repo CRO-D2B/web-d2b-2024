9ffc9b76f48fd4ec975beedafe7471eff567f70d
FabianMorag <fabianmorag0405@gmail.com>
2024-04-22 10:59:24 -0400

    contant menu items

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const menuItems = [
+  { name: 'Inicio', slug: '#' },
+  { name: 'Servicios', slug: '#' },
+  { name: 'Casos', slug: '#' },
+  { name: 'Blog', slug: '/blog' },
+  { name: 'Contacto', slug: '/contacto' },
+  { name: 'Comunidad D2B', slug: '#' },
+  { name: 'Trabaja con nosotros', slug: '#' }
+]
