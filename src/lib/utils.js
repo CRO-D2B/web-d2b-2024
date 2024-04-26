b3d58bc7b5561d00d0bb184f90c62a93602bd263
FabianMorag <fabianmorag0405@gmail.com>
2024-04-26 12:19:25 -0400

    categories pages & padding inline fix in all pages

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,2 +1,16 @@
 export const isDev = import.meta.env.DEV
 export const isProd = import.meta.env.PROD
+
+export const getCaption = (post) => {
+  const { caption } = post.yoast_head_json.schema['@graph'].filter(
+    (item) => item['@type'] === 'ImageObject'
+  )[0]
+  return caption ?? `Imagen de ${post.title.rendered}`
+}
+
+export const getImgUrl = (post) => {
+  const { url } = post.yoast_head_json.schema['@graph'].filter(
+    (item) => item['@type'] === 'ImageObject'
+  )[0]
+  return url ?? post.yoast_head_json.og_image[0].url
+}
--- src/services/blog.js
+++ src/services/blog.js
@@ -2,11 +2,20 @@ const WP_API_URL = 'https://d2b.cl/blog/wp-json/wp/v2'
 
 export const getCategories = async () => {
   const endpoint = '/categories'
-  const params = '_fields=name,slug'
+  const params = '_fields=id,name,slug'
   const url = buildUrl({ endpoint, params })
   const res = await fetch(url)
   const data = await res.json()
   return data
+}
+
+export const getCategoryInfo = async ({ categorySlug }) => {
+  const endpoint = '/categories'
+  const params = `slug=${categorySlug}`
+  const url = buildUrl({ endpoint, params })
+  const res = await fetch(url)
+  const data = await res.json()
+  return data[0]
 }
 
 export const getPosts = async () => {
@@ -17,9 +26,17 @@ export const getPosts = async () => {
   const data = await res.json()
   return data
 }
-export const getPostsBySlug = async ({ postSlug }) => {
+export const getPostBySlug = async ({ postSlug }) => {
   const endpoint = '/posts'
   const params = `slug=${postSlug}`
+  const url = buildUrl({ endpoint, params })
+  const res = await fetch(url)
+  const data = await res.json()
+  return data[0]
+}
+export const getPostsByCategoryId = async ({ id }) => {
+  const endpoint = '/posts'
+  const params = `categories=${id}&per_page=100`
   const url = buildUrl({ endpoint, params })
   const res = await fetch(url)
   const data = await res.json()
