0e836b9c6f15540126dd6b378009212e9a593148
FabianMorag <fabianmorag0405@gmail.com>
2024-05-09 15:13:28 -0400

    items reorder & "comunidad" removed

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,8 +2,7 @@ export const menuItems = [
   { name: 'Inicio', slug: '/' },
   { name: 'Servicios', slug: '#' },
   { name: 'Casos', slug: '#' },
-  { name: 'Blog', slug: '/blog' },
   { name: 'Contacto', slug: '/contacto' },
-  { name: 'Comunidad D2B', slug: '#' },
-  { name: 'Trabaja con nosotros', slug: '#' }
+  { name: 'Trabaja con nosotros', slug: '#' },
+  { name: 'Blog', slug: '/blog' }
 ]
