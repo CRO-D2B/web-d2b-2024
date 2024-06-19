fc707f42a69c4a1e2ec5a24d837c15dc907f6953
FabianMorag <fabianmorag0405@gmail.com>
2024-06-19 15:03:13 -0400

    page created: trabaja con nosotros

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,13 +3,13 @@ export const headerMenuItems = [
   { name: 'Servicios', slug: '/servicios' },
   { name: 'Casos', slug: '#' },
   { name: 'Contacto', slug: '/contacto' },
-  { name: 'Trabaja con nosotros', slug: '#' },
+  { name: 'Trabaja con nosotros', slug: '/trabaja-con-nosotros' },
   { name: 'Blog', slug: '/blog' }
 ]
 
 export const footerMenuItems = [
   { name: 'Home', slug: '/' },
-  { name: 'Trabaja con nosotros', slug: '#' },
+  { name: 'Trabaja con nosotros', slug: '/trabaja-con-nosotros' },
   { name: 'Servicios', slug: '/servicios' },
   { name: 'Blog', slug: '/blog' },
   { name: 'Casos', slug: '#' },
--- src/services/contact.js
+++ src/services/contact.js
@@ -2,3 +2,5 @@ import { client } from './config'
 import { readSingleton } from '@directus/sdk'
 
 export const contactPage = await client.request(readSingleton('Contacto'))
+
+export const workWithUsPage = await client.request(readSingleton('Landing_trabaja_con_nosotros'))
