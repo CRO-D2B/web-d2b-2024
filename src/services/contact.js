e40820324d79f590d1e03b1b06d7e89799543b83
FabianMorag <fabianmorag0405@gmail.com>
2024-06-21 11:03:17 -0400

    d2b benefits showed

--- src/services/contact.js
+++ src/services/contact.js
@@ -1,6 +1,8 @@
 import { client } from './config'
-import { readSingleton } from '@directus/sdk'
+import { readItems, readSingleton } from '@directus/sdk'
 
 export const contactPage = await client.request(readSingleton('Contacto'))
 
 export const workWithUsPage = await client.request(readSingleton('Landing_trabaja_con_nosotros'))
+
+export const benefits = await client.request(readItems('Beneficios'))
