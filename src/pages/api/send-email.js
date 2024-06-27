488321e45dd999709dd0818fdc5d83fe8d8aa07e
FabianMorag <fabianmorag0405@gmail.com>
2024-06-27 17:23:27 -0400

    email fixes

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -1,26 +1,22 @@
 import { Resend } from 'resend'
-import { isDev } from '@/lib/utils'
 
-const devTag = isDev ? 'DEV | ' : ''
-
-export const POST = async ({ params, request }) => {
+export const POST = async ({ _params, request }) => {
   const { name, email, number, company, message } = await request.json()
 
   const resend = new Resend(import.meta.env.RESEND_API_KEY)
-  try {
-    await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: 'fabianmorag0405@gmail.com',
-      subject: `${devTag}Hello World`,
-      html: `
+  const { _data, error } = await resend.emails.send({
+    from: 'ContactoD2B <onboarding@resend.dev>',
+    to: 'fabianmorag0405@gmail.com',
+    subject: 'Desde d2b.cl',
+    html: `
         <strong>Nombre</strong>: ${name} <br>
         <strong>Email</strong>: ${email} <br>
         <strong>Telefono</strong>: ${number} <br>
         <strong>Empresa</strong>: ${company} <br>
         <strong>Comentario</strong>: ${message} <br>
       `
-    })
-  } catch (error) {
+  })
+  if (error) {
     console.error(error)
     return new Response('Error sending email', { status: 500 })
   }
