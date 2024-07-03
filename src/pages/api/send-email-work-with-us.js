e1032433be90011ee7d24d38d3cd556979c941e2
FabianMorag <fabianmorag0405@gmail.com>
2024-07-03 16:32:18 -0400

    error message and success message for contact forms

--- src/pages/api/send-email-work-with-us.js
+++ src/pages/api/send-email-work-with-us.js
@@ -1,7 +1,7 @@
 import { Resend } from 'resend'
 
 export const POST = async ({ _params, request }) => {
-  const { name, email, number, company, message } = await request.json()
+  const { name, email, linkedinProfile, interest } = await request.json()
 
   const resend = new Resend(import.meta.env.RESEND_API_KEY)
   const { error } = await resend.emails.send({
@@ -11,11 +11,11 @@ export const POST = async ({ _params, request }) => {
     html: `
         <strong>Nombre</strong>: ${name} <br>
         <strong>Email</strong>: ${email} <br>
-        <strong>Telefono</strong>: ${number} <br>
-        <strong>Empresa</strong>: ${company} <br>
-        <strong>Comentario</strong>: ${message} <br>
+        <strong>LinkedIn</strong>: ${linkedinProfile} <br>
+        <strong>Área de interes</strong>: ${interest} <br>
       `
   })
+
   if (error) {
     console.error(error)
     return new Response('Error sending email', { status: 500 })
