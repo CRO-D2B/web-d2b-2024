b013ffd7e139e4baa061a4a0994dca9a498c6035
FabianMorag <fabianmorag0405@gmail.com>
2024-07-15 12:41:07 -0400

    success stories page added

--- src/services/home.js
+++ src/services/home.js
@@ -10,5 +10,3 @@ export const clients = await client.request(readItems('Clientes'))
 export const partners = await client.request(readItems('Partners'))
 
 export const team = await client.request(readItems('Equipo'))
-
-export const cases = await client.request(readItems('Casos'))
---
+++ src/services/successStories.js
@@ -0,0 +1,13 @@
+import { client } from '@/lib/resend'
+import { readSingleton, readItems } from '@directus/sdk'
+
+export const successStoryLanding = await client.request(readSingleton('Landing_casos'))
+
+export const successStories = await client.request(readItems('Casos'))
+
+export const successStoryByName = (slug) => client.request(readItems('Casos',
+  {
+    fields: ['*'],
+    filter: { slug }
+  }
+))
