80dbb8de9b0461453859406c7771776cfaa26e67
FabianMorag <fabianmorag0405@gmail.com>
2024-07-24 09:24:26 -0400

    little fix

--- src/pages/api/deploy.js
+++ src/pages/api/deploy.js
@@ -1,12 +1,12 @@
 import { exec } from 'node:child_process'
 
-const deployFile = '_deploy.sh'
+const deployCmd = 'sh src/pages/api/_deploy.sh'
 
 export const POST = async ({ params, request }) => {
   // if (!request.url.includes('github')) { return new Response('Not valid request', { status: 500 }) }
 
   exec(
-    `sh src/pages/api/${deployFile}`,
+    deployCmd,
     (error, stdout, stderr) => {
       console.log(stdout)
       console.log(stderr)
