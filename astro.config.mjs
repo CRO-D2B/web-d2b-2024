241f230b73c8807cdd3670d84ddd3b2844c20374
FabianMorag <fabianmorag0405@gmail.com>
2024-07-25 16:03:46 -0400

    canonical correctly set

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: import.meta.env.BASE_URL,
   output: 'server',
   adapter: node({
     mode: 'standalone'
   })
-});
+})
