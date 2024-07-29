3adcd8aa7d171573b8d34f02210c7c8cb02b5340
FabianMorag <fabianmorag0405@gmail.com>
2024-07-29 18:02:02 -0400

    deploy script updated

--- src/pages/api/_deploy.sh
+++ src/pages/api/_deploy.sh
@@ -1,4 +1,21 @@
-git pull
-pnpm i
-pnpm run build
-pnpm run restart-deploy
+echo "Rebuild iniciado en $(date)"
+
+# Definir la ruta del proyecto
+PROJECT_DIR=/var/www/dev.d2b.cl/
+
+# Navegar al directorio del proyecto
+cd $PROJECT_DIR || { echo "No se pudo acceder al directorio del proyecto"; exit 1; }
+
+# Actualizar el repositorio
+git pull || { echo "Error al hacer git pull"; exit 1; }
+
+# Instalar dependencias
+pnpm install || { echo "Error al instalar dependencias"; exit 1; }
+
+# Construir el proyecto 
+pnpm build || { echo "Error al hacer build del proyecto"; exit 1; }
+
+# Reiniciar el proyecto en pm2
+pnpm restart-deploy || { echo "Error al reiniciar el proyecto en pm2"; exit 1; }
+
+echo "Rebuild finalizado en $(date)"
