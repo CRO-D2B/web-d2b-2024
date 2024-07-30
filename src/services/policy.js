6eae651e2792b960b1085cb1c9838458b25647e2
FabianMorag <fabianmorag0405@gmail.com>
2024-07-30 17:34:26 -0400

    legal notices landing modified

--- src/services/policy.js
+++ src/services/policy.js
@@ -2,3 +2,5 @@ import { client } from '@/lib/resend'
 import { readSingleton } from '@directus/sdk'
 
 export const policyLanding = await client.request(readSingleton('Landing_politicas'))
+
+export const legalLanding = await client.request(readSingleton('Landing_avisos_legales'))
