bef8f16686514c78246e76e609d0676665eb7ca8
FabianMorag <fabianmorag0405@gmail.com>
2024-07-31 10:37:03 -0400

    sitemap config added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import node from '@astrojs/node'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   // site: 'https://d2b.cl',
   output: 'server',
   adapter: node({
