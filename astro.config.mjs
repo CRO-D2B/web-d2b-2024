082a68c834624c902568a15d21bb0ee779a086b2
FabianMorag <fabianmorag0405@gmail.com>
2024-08-06 11:25:13 -0400

    seo config pre prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,27 +7,16 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
-  // site: 'https://d2b.cl',
+  site: 'https://d2b.cl',
   output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
   redirects: {
-    '/blog/analitica-web': {
-      status: 301,
-      destination: '/blog/categoria/analitica-web'
-    },
-    '/blog/marketing-digital': {
-      status: 301,
-      destination: '/blog/categoria/marketing-digital'
-    },
-    '/blog/cro': {
-      status: 301,
-      destination: '/blog/categoria/cro'
-    },
-    '/blog/posicionamiento-web': {
-      status: 301,
-      destination: '/blog/categoria/posicionamiento-web'
-    }
+    '/blog/analitica-web': { status: 301, destination: '/blog/categoria/analitica-web' },
+    '/blog/marketing-digital': { status: 301, destination: '/blog/categoria/marketing-digital' },
+    '/blog/cro': { status: 301, destination: '/blog/categoria/cro' },
+    '/blog/posicionamiento-web': { status: 301, destination: '/blog/categoria/posicionamiento-web' },
+    '/blog/trabaja-con-nosotros': { status: 301, destination: '/quienes-somos' }
   }
 })
--- src/pages/robots.txt.js
+++ src/pages/robots.txt.js
@@ -1,6 +1,28 @@
 const robotsTxt = `
 User-agent: *
-Allow: /
+
+Allow: /wp-admin/admin-ajax.php
+Disallow: /wp-login
+Disallow: /wp-admin
+Disallow: /*/feed/
+Disallow: /*/trackback/
+Disallow: /*/attachment/
+Disallow: *?replytocom
+Disallow: /tag/*/page/
+Disallow: /tag/*/feed/
+Disallow: /comments/
+Disallow: /xmlrpc.php
+Disallow: /*?s=
+Disallow: /*/*/*/feed.xml
+Disallow: /?attachment_id*
+Disallow: /page
+Disallow: /tag.*
+Disallow: /blog/tag/
+Disallow: /blog_d2b/
+
+Allow: /*.css$
+Allow: /*.js$
+
 Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
 `.trim()
 
